c0a6c1c708abb443283d096f2085caa9a3a8eb48
Michal Witas <bolek117@interia.pl>
2015-03-13 15:11:50 +0100

    Work in progress

--- vigenere_crack/vigenere_crack.js
+++ vigenere_crack/vigenere_crack.js
@@ -25,34 +25,59 @@ function d(obj) {
 }
 
 var Vigenere = {
-    decrypt: function(encrypted, key) {
-        // Source: http://www.nayuki.io/res/vigenere-cipher-javascript.js
-        for (var i = 0; i < key.length; i++)
-            key[i] = (26 + key[i]) % 26;
-        
-        return Vigenere.encrypt(encrypted, key);
+    decrypt: function(input, key) {
+        key = Vigenere.preprocessKey(key, true);
+        return Vigenere.calculate(input, key);
     },    
+    
     encrypt: function(input, key) {
-        // Source: http://www.nayuki.io/res/vigenere-cipher-javascript.js
+        key = Vigenere.preprocessKey(key, false);
+        return Vigenere.calculate(input, key);
+    },
+    
+    calculate: function(input, key) {
         var output = "";
         for (var i = 0, j = 0; i < input.length; i++) {
             var c = input.charCodeAt(i);
-            var diff, shouldRewrite = true;
-            if (isUppercase(c)) {
-                diff = 65;
-            } else if (isLowercase(c)) {
-                diff = 97;
-            } else {
+            var base = 0;
+            
+            if (isUppercase(c))
+                base = 'A'.charCodeAt(0);
+            else if (isLowercase(c))
+                base = 'a'.charCodeAt(0);
+            else {
                 output += input.charAt(i);
                 continue;
             }
-                
-            output += String.fromCharCode((c + key.charCodeAt(j%key.length) - diff - 65) % 26 + diff);
+            
+            if (Settings.mode.actual === Settings.mode.decrypt && c === 65)
+                console.log('test');
+            
+            
+            var a = (c + key[j%key.length] - base);
+            var b = a%26;
+            var d = b + base;
+            output += String.fromCharCode(d);
             j++;
         }
         
         return output;
-    }    
+    },
+    
+    preprocessKey: function(key, toDecrypt) {
+        key = key.toUpperCase();
+        var output = [];
+        for(var i=0; i<key.length; ++i) {
+            var val = key.charCodeAt(i) - 'A'.charCodeAt(0);
+            
+            if (toDecrypt === true)
+                val *= -1;
+            
+            output.push(val);
+        }
+        
+        return output;
+    }
 }
 
 var Listeners = {
@@ -71,8 +96,13 @@ var Listeners = {
             var encrypted = $(Selectors.encryptedText).val();
             var key = $(Selectors.encryptionKey).val().toUpperCase();
 
-            var decrypted = Vigenere.encrypt(encrypted, key);
-            $(Selectors.decryptedText).val(decrypted);
+            if (Settings.mode.actual === Settings.mode.encypt) {
+                var result = Vigenere.encrypt(encrypted, key);
+            } else {
+                var result = Vigenere.decrypt(encrypted, key);
+            }
+            
+            $(Selectors.decryptedText).val(result);
         }
     },
     
@@ -80,14 +110,13 @@ var Listeners = {
         Settings.mode.actual = Settings.mode.encypt;
         
         $(Selectors.modeSelector).on('click', function() {
-            if (Settings.mode.actual === Settings.mode.encypt) {
-                Settings.mode.actual = Settings.mode.decrypt;
-            } else {
-                Settings.mode.actual = Settings.mode.encypt;
-            }
+            Settings.mode.actual = (Settings.mode.actual === Settings.mode.encypt) 
+                ? Settings.mode.decrypt
+                : Settings.mode.encypt;
             
-            
-            d($(Selectors.modeSelector).innerHTML);
+            $(Selectors.modeSelector).val(Settings.mode.actual);
+            $(Selectors.modeSelector).html('Mode: ' + Settings.mode.actual);
+            $(Selectors.encryptedText).trigger('change');
         })
     }
 }
